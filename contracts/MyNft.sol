f59f01c918f01661b1d650c53253e25f0abfdc69
Jisu Kim <webmaster@alien.moe>
2021-10-06 12:47:44 +0900

    feat: add test

--- contracts/MyNft.sol
+++ contracts/MyNft.sol
@@ -1,6 +1,10 @@
 pragma solidity 0.5.6;
-import '@klaytn/contracts/token/KIP17/KIP17.sol';
+import '@klaytn/contracts/token/KIP17/KIP17Token.sol';
 
-contract MyNft is KIP17 {
-  constructor() public {}
+contract MyNft is KIP17Token {
+  // name과 symbol을 초기화
+  constructor(string memory name, string memory symbol)
+    public
+    KIP17Token(name, symbol)
+  {}
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,23 +2,5 @@ const MyNft = artifacts.require('./MyNft.sol');
 const fs = require('fs');
 
 module.exports = function (deployer) {
-  deployer.deploy(MyNft).then(() => {
-    if (MyNft._json) {
-      // 1. Record recently deployed contract's abi file to 'deployedABI'
-      fs.writeFile('deployedABI', JSON.stringify(MyNft._json.abi, 2), err => {
-        if (err) throw err;
-        console.log(
-          `The abi of ${MyNft._json.contractName} is recorded on deployedABI file`
-        );
-      });
-    }
-
-    // 2. Record recently deployed contract's address to 'deployedAddress'
-    fs.writeFile('deployedAddress', MyNft.address, err => {
-      if (err) throw err;
-      console.log(
-        `The deployed contract address * ${MyNft.address} * is recorded on deployedAddress file`
-      );
-    });
-  });
+  deployer.deploy(MyNft, 'MyKlay', 'MKlay');
 };
